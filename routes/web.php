eed51c3705067f80b77874614809fad7a2ae613b
Kresna Satya <kresnasatya72@gmail.com>
2024-05-31 06:42:55 +0800

    convert welcome blade view to welcome svelte page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION
+    ]);
 });
 
 Route::get('/dashboard', function () {
