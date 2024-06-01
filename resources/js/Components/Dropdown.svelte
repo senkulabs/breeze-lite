8179fd8558142b35dbff51b8d30ea30a8a05728c
Kresna Satya <kresnasatya72@gmail.com>
2024-06-01 10:44:39 +0800

    fix dropdown and dropdown link components

--- resources/js/Components/Dropdown.svelte
+++ resources/js/Components/Dropdown.svelte
@@ -1,9 +1,6 @@
 <script>
     import { onMount, onDestroy } from 'svelte';
 
-    export let align = 'right';
-    export let width = '48';
-
     const closeOnEscape = (e) => {
         if (open.value && e.key === 'Escape') {
             open.value = false;
@@ -13,16 +10,21 @@
     onMount(() => document.addEventListener('keydown', closeOnEscape));
     onDestroy(() => document.addEventListener('keydown', closeOnEscape));
 
-    let widthClass;
-    $: widthClass = {
-        48: "w-48",
-    }[width.toString()];
-
-    let alignmentClass;
-    $: alignmentClass =
-        align === 'left' ? 'origin-top-left left-0' :
-        align === 'right' ? 'origin-top-right right-0' :
-        'origin-top';
+    export let width = 48;
+    export let align;
+    let alignmentClasses;
+    switch (align) {
+        case 'left':
+            alignmentClasses = 'ltr:origin-top-left rtl:origin-top-right start-0';
+            break;
+        case 'top':
+            alignmentClasses = 'origin-top';
+            break;
+        case 'right':
+        default:
+            alignmentClasses = 'ltr:origin-top-right rtl:origin-top-left end-0';
+            break;
+    }
 
     let open = false;
 </script>
@@ -35,10 +37,7 @@
     <!-- Full Screen Dropdown Overlay -->
     {#if open}
         <button class="fixed inset-0 z-40 select-none" on:click={() => open = false}></button>
-    {/if}
-
-    {#if open}
-        <button class={`absolute z-50 mt-2 rounded-md shadow-lg ${ widthClass + " " + alignmentClass }`}
+        <button class="absolute z-50 mt-2 rounded-md shadow-lg w-{width} {alignmentClasses}"
         on:click={() => open = false}>
             <div class={`rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white dark:bg-gray-700`}>
                 <slot name="content"/>
--- resources/js/Components/DropdownLink.svelte
+++ resources/js/Components/DropdownLink.svelte
@@ -1,9 +1,15 @@
 <script>
     import { inertia } from "@inertiajs/svelte";
 
-    export let href, method, as;
+    export let href = '#', method = 'get', type;
 </script>
 
-<a use:inertia {href} {method} {as} class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
-    <slot/>
-</a>
+{#if type === "button"}
+    <button use:inertia={{ href: href, method: method }} type="button" class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
+        <slot/>
+    </button>
+{:else}
+    <a use:inertia {href} {method} class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
+        <slot/>
+    </a>
+{/if}
