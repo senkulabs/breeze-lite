294148fc0e9146a11008b749ab743c51eee33a6b
Kresna Satya <kresnasatya72@gmail.com>
2024-11-03 12:43:56 +0800

    remove resolve from vite.config.js

--- stubs/inertia-svelte/vite.config.js
+++ stubs/inertia-svelte/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,12 +9,5 @@ export default defineConfig({
             refresh: true,
         }),
         svelte()
-    ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
-        extensions: ['.js', '.svelte', '.json']
-    }
+    ]
 });
