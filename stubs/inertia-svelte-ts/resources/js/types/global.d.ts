989c2347bee5ae4b00ba3a2a83241bb4fd9e22a5
Ed Johnson-Williams <edjw@users.noreply.github.com>
2024-11-04 13:14:28 +0000

    add global types for ziggy route function

--- stubs/inertia-svelte-ts/resources/js/types/global.d.ts
+++ stubs/inertia-svelte-ts/resources/js/types/global.d.ts
@@ -1,13 +1,20 @@
 import { PageProps as InertiaPageProps } from '@inertiajs/core';
 import { AxiosInstance } from 'axios';
 import { PageProps as AppPageProps } from './';
+import type { Config, RouteParam } from "ziggy-js";
 
 declare global {
     interface Window {
         axios: AxiosInstance
-    }
+    };
+    function route(
+        name: string,
+        params?: RouteParam,
+        absolute?: boolean,
+        config?: Config
+    ): string;
 }
 
 declare module '@inertiajs/core' {
-    interface PageProps extends InertiaPageProps, AppPageProps {}
+    interface PageProps extends InertiaPageProps, AppPageProps { }
 }
