500c33fefd8bf0de2c986e109540ff705dade8e9
Kresna Satya <kresnasatya72@gmail.com>
2024-11-10 12:12:59 +0800

    fix modal in svelte js template

--- stubs/inertia-svelte/resources/js/Components/Modal.svelte
+++ stubs/inertia-svelte/resources/js/Components/Modal.svelte
@@ -1,11 +1,12 @@
 <script>
     import { onDestroy } from 'svelte';
-    import { derived } from 'svelte/store';
 
+    let dialog;
     let { children, closeable = true, maxWidth = '2xl', onclose = () => {}, show = false } = $props();
 
     $effect(() => {
         document.body.style.overflow = show ? 'hidden' : 'visible';
+        document.body.appendChild(dialog);
     });
 
     const close = () => {
@@ -22,6 +23,7 @@
 
     onDestroy(() => {
         document.removeEventListener('keydown', closeOnEscape);
+        document.body.removeChild(dialog);
         document.body.style.overflow = 'visible';
     });
 
@@ -38,15 +40,15 @@
 
 <svelte:window onkeydown={closeOnEscape}/>
 
-{#if show}
-<div class="fixed inset-0 z-50 px-4 py-6 overflow-y-auto sm:px-0">
-    <!-- svelte-ignore a11y_consider_explicit_label -->
-    <button class="fixed inset-0 transition-all transform-select-none" onclick={close()}>
-        <div class="absolute inset-0 bg-gray-500 opacity-75 dark:bg-gray-900">&nbsp;</div>
-    </button>
+<div bind:this={dialog} style:display={show ? 'contents' : 'none'}>
+    <div class="fixed inset-0 z-50 px-4 py-6 overflow-y-auto sm:px-0">
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+        <button class="fixed inset-0 transition-all transform-select-none" onclick={close}>
+            <div class="absolute inset-0 bg-gray-500 opacity-75 dark:bg-gray-900">&nbsp;</div>
+        </button>
 
-    <div class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto {maxWidthClass}">
-        {@render children()}
+        <div class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto {maxWidthClass}">
+            {@render children()}
+        </div>
     </div>
 </div>
-{/if}
--- stubs/inertia-svelte/resources/js/Pages/Profile/Partials/DeleteUserForm.svelte
+++ stubs/inertia-svelte/resources/js/Pages/Profile/Partials/DeleteUserForm.svelte
@@ -6,7 +6,6 @@
     import SecondaryButton from "@/Components/SecondaryButton.svelte";
     import TextInput from "@/Components/TextInput.svelte";
     import { useForm } from "@inertiajs/svelte";
-    
 
     let confirmingUserDeletion = $state(false);
 
@@ -53,55 +52,53 @@
 
     <DangerButton type="button" onclick={confirmUserDeletion}>Delete Account</DangerButton>
 
-    {#if confirmingUserDeletion}
-        <Modal onclose={closeModal} show={confirmUserDeletion}>
-            <div class="p-6">
-                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                    Are you sure you want to delete your account?
-                </h2>
+    <Modal onclose={closeModal} show={confirmingUserDeletion}>
+        <div class="p-6">
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                Are you sure you want to delete your account?
+            </h2>
 
-                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Once your account is deleted, all of its resources and data
-                    will be permanently deleted. Please enter your password to
-                    confirm you would like to permanently delete your account.
-                </p>
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Once your account is deleted, all of its resources and data
+                will be permanently deleted. Please enter your password to
+                confirm you would like to permanently delete your account.
+            </p>
 
-                <div class="mt-6">
-                    <InputLabel
-                        for="password"
-                        value="Password"
-                        class="sr-only"
-                    />
+            <div class="mt-6">
+                <InputLabel
+                    for="password"
+                    value="Password"
+                    class="sr-only"
+                />
 
-                    <TextInput
-                        id="password"
-                        bind:this={passwordInput}
-                        bind:value={$form.password}
-                        type="password"
-                        class="mt-1 block w-full"
-                        placeholder="Password"
-                        onkeyup={(event) => {
-                            if (event.key === "Enter") deleteUser();
-                        }}
-                    />
+                <TextInput
+                    id="password"
+                    bind:this={passwordInput}
+                    bind:value={$form.password}
+                    type="password"
+                    class="mt-1 block w-full"
+                    placeholder="Password"
+                    onkeyup={(event) => {
+                        if (event.key === "Enter") deleteUser();
+                    }}
+                />
 
-                    <InputError message={$form.errors.password} class="mt-2" />
-                </div>
-
-                <div class="mt-6 flex justify-end">
-                    <SecondaryButton onclick={closeModal}>
-                        Cancel
-                    </SecondaryButton>
-
-                    <DangerButton
-                        class="ml-3"
-                        processing={$form.processing}
-                        onclick={deleteUser}
-                    >
-                        Delete Account
-                    </DangerButton>
-                </div>
+                <InputError message={$form.errors.password} class="mt-2" />
             </div>
-        </Modal>
-    {/if}
+
+            <div class="mt-6 flex justify-end">
+                <SecondaryButton onclick={closeModal}>
+                    Cancel
+                </SecondaryButton>
+
+                <DangerButton
+                    class="ml-3"
+                    processing={$form.processing}
+                    onclick={deleteUser}
+                >
+                    Delete Account
+                </DangerButton>
+            </div>
+        </div>
+    </Modal>
 </section>
