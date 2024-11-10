1d2bc10a7036b212352f405dbc156d67a1735d7b
Kresna Satya <kresnasatya72@gmail.com>
2024-11-10 20:24:03 +0800

    remove svelte third party library from several components and use svelte itself
    - Remove third party: svelte-transition, svelte-portal
    - Use svelte/transition and svelte/easing to create the same transition experience

--- stubs/inertia-svelte/resources/js/Components/Dropdown.svelte
+++ stubs/inertia-svelte/resources/js/Components/Dropdown.svelte
@@ -1,39 +1,83 @@
 <script>
-    let { width = 48, align = 'right', content, contentClasses = 'py-1 bg-white dark:bg-gray-700', trigger } = $props();
+    import { fade } from 'svelte/transition';
+    import { cubicIn, cubicOut } from 'svelte/easing';
+
+    let {
+        align = 'right',
+        content,
+        contentClasses = 'py-1 bg-white dark:bg-gray-700',
+        trigger,
+        width = '48',
+    } = $props();
 
     let open = $state(false);
-    let widthClass = $derived({ '48': 'w-48'}[width]);
+    let widthClass = $derived({ '48': 'w-48' }[width]);
     let alignmentClasses = $derived(
         align === 'left'
-        ? 'ltr:origin-top-left rtl:origin-top-right start-0'
-        : align === 'right'
-            ? 'ltr:origin-top-right rtl:origin-top-left end-0'
-            : 'origin-top',
-    );
+            ? 'ltr:origin-top-left rtl:origin-top-right start-0'
+            : align === 'right'
+              ? 'ltr:origin-top-right rtl:origin-top-left end-0'
+              : 'origin-top',
+    )
 
-    function closeOnEscape(event) {
-        if (open && event.key === 'Escape') {
-            open = false;
+    function closeOnEscape(e) {
+        if (open && e.key === 'Escape') {
+            open = false
         }
-    };
+    }
+
+    function enter(node, {
+        delay = 0,
+        duration = 200,
+        easing = cubicOut
+    }) {
+        return {
+            delay,
+            duration,
+            easing,
+            css: (t) => `
+                opacity: ${t};
+                transform: scale(${0.95 + 0.05 * t});
+            `
+        }
+    }
+
+    function leave(node, {
+        delay = 0,
+        duration = 75,
+        easing = cubicIn
+    }) {
+        return {
+            delay,
+            duration,
+            easing,
+            css: (t) => `
+                opacity: ${t};
+                transform: scale(${0.95 + 0.05 * t});
+            `
+        }
+    }
 </script>
 
-<svelte:window onkeydown={closeOnEscape}></svelte:window>
+<svelte:window on:keydown={closeOnEscape} />
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class="relative">
-    <button onclick={() => open = !open}>
+    <div onclick={() => (open = !open)}>
         {@render trigger()}
-    </button>
+    </div>
 
     <!-- Full Screen Dropdown Overlay -->
     {#if open}
-        <!-- svelte-ignore a11y_consider_explicit_label -->
-        <button class="fixed inset-0 z-40 select-none" onclick={() => open = false}></button>
-        <button class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
-        onclick={() => open = false}>
-            <div class="rounded-md ring-1 ring-black ring-opacity-5 {contentClasses}">
-                {@render content()}
+        <div class="fixed inset-0 z-40" onclick={() => (open = false)}></div>
+        <div in:enter out:leave>
+            <div class="absolute z-50 mt-2 rounded-md shadow-lg {widthClass} {alignmentClasses}"
+            onclick={() => (open = false)}>
+                <div class="rounded-md ring-1 ring-black ring-opacity-5 {contentClasses}">
+                    {@render content()}
+                </div>
             </div>
-        </button>
+        </div>
     {/if}
 </div>
