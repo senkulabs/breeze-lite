d17c322988a4cbb449701625f08fe1fcd65f740c
Kresna Satya <satyakresna@unud.ac.id>
2024-11-11 14:38:31 +0800

    fix: add children props into GuestLayout

--- stubs/inertia-svelte-ts/resources/js/Layouts/GuestLayout.svelte
+++ stubs/inertia-svelte-ts/resources/js/Layouts/GuestLayout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import type { Snippet } from 'svelte';
     import { inertia } from '@inertiajs/svelte';
     import ApplicationLogo from '@/Components/ApplicationLogo.svelte';
+
+    let { children }: { children: Snippet } = $props();
 </script>
 
 <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
--- stubs/inertia-svelte/resources/js/Layouts/GuestLayout.svelte
+++ stubs/inertia-svelte/resources/js/Layouts/GuestLayout.svelte
@@ -1,6 +1,8 @@
 <script>
     import ApplicationLogo from '@/Components/ApplicationLogo.svelte';
     import { inertia } from '@inertiajs/svelte';
+
+    let { children } = $props();
 </script>
 
 <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
