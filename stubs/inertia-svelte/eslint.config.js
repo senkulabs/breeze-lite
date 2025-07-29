42c553ce381a5d359396d27c45eb1b9b46990581
Kresna Satya <kresnasatya72@gmail.com>
2025-07-29 08:57:36 +0800

    fix: tweak eslint config in order to remove false svelte/valid-compile
    One of them:
    
    /Users/kresnasatya/Sandbox/inertia2-svelte5-issue/resources/js/Components/TextInput.svelte
      8:9  error  Using a rest element or a non-destructured declaration with `$props()` means that Svelte can't infer what properties to expose when creating a custom element. Consider destructuring all the props or explicitly specifying the `customElement.props` option.
    https://svelte.dev/e/custom_element_props_identifier(custom_element_props_identifier)  svelte/valid-compile

--- stubs/inertia-svelte/eslint.config.js
+++ stubs/inertia-svelte/eslint.config.js
@@ -2,6 +2,7 @@ import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import svelteConfig from './svelte.config.js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -17,6 +18,18 @@ export default [
 			}
 		}
 	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.js'],
+		languageOptions: {
+			parserOptions: {
+				// We recommend importing and specifying svelte.config.js.
+				// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+				// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+				// explicitly specifying it ensures better compatibility and functionality.
+				svelteConfig
+			}
+		}
+  	},
 	{
 		ignores: ['bootstrap', 'public', 'vendor']
 	},
